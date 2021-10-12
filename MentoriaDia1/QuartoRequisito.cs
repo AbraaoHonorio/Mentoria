b4e838cbdcc3baebefac845b7bc5535ee30560d9
Ruâni Filipe de Albuquerque <71514696+RuaniFilipe@users.noreply.github.com>
2021-10-12 17:39:29 -0300

    Requisito 4 - ok

---
+++ MentoriaDia1/QuartoRequisito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ListaExerciciosMentoria.MentoriaDia1
+{
+    class QuartoRequisito
+    {
+        public static void Executar()
+        {
+            string resposta;
+            Console.Write("Escreva um número: ");
+            int.TryParse(Console.ReadLine(), out int num);
+
+            resposta = Math.Abs(num) % 2 == 0 ? "par" : "ímpar";
+
+            Console.WriteLine($"{Math.Abs(num)} é um número {resposta}");
+        }
+    }
+}
