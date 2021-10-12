342f474c41b9389594ead33b082f912e9aa50e8f
Ruâni Filipe de Albuquerque <71514696+RuaniFilipe@users.noreply.github.com>
2021-10-12 16:55:11 -0300

    Adicionar arquivos de projeto.

---
+++ MentoriaDia1/PrimeiroRequisito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExerciciosMentoria.MentoriaDia1
+{
+    class PrimeiroRequisito
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ListaExerciciosMentoria.MentoriaDia1;
+
+namespace ListaExerciciosMentoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Requisito 1", PrimeiroRequisito.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
