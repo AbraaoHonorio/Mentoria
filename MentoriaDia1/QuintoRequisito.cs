675b636f1707ec8f776c32ab46b5739aeb05bd43
Ruâni Filipe de Albuquerque <71514696+RuaniFilipe@users.noreply.github.com>
2021-10-13 14:54:01 -0300

    Requisito 5 - ok

---
+++ MentoriaDia1/QuintoRequisito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExerciciosMentoria.MentoriaDia1
+{
+    class QuintoRequisito
+    {
+        public static void Executar()
+        {
+            Console.Write("Quantas maçãs foram compradas? ");
+            int.TryParse(Console.ReadLine(), out int quantidadeComprada);
+            
+            var valorUnidadeComprada = quantidadeComprada >= 12 ? 0.25 : 0.30;
+
+            double valorTotalCompra = quantidadeComprada * valorUnidadeComprada;
+                        
+            Console.WriteLine($"Você comprou {quantidadeComprada} maçãs, a R$ {valorUnidadeComprada:F2} cada uma, dando um total de compra de R$ {valorTotalCompra:F2}.");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace ListaExerciciosMentoria
                 {"Requisito 2", SegundoRequisito.Executar},
                 {"Requisito 3", TerceiroRequisito.Executar},
                 {"Requisito 4", QuartoRequisito.Executar},
+                {"Requisito 5", QuintoRequisito.Executar},
             });
 
             central.SelecionarEExecutar();
